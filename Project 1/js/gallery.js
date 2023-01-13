1d6b365aef3d586145dbf2b672732ead441cfb9d
Chang Liu <changstudy19@gmail.com>
2023-01-13 12:12:04 +1030

    first time gallery lightbox

--- Project 1/js/gallery.js
+++ Project 1/js/gallery.js
@@ -1,14 +1,26 @@
-window.addEventListener('load', function() {
-    let thumbnail = document.querySelector(".gallery-item-inner");
-    let overlay = document.querySelector(".overlay");
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
 
-    thumbnail.addEventListener("mouseenter", function() {
-        overlay.style.display = "table";
+const gallery = document.getElementById('gallery-outer');
+const overlays = gallery.querySelectorAll('h4');
+overlays.forEach(overlay => {
+    overlay.addEventListener('click', e => {
+        lightbox.classList.add('active');
+        const img = document.createElement('img');
+        const image = overlay.parentElement.parentElement.getElementsByTagName('img')[0];
+        img.src = image.src;
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild);
+        }
+        lightbox.appendChild(img);
     })
-    thumbnail.addEventListener("mouseleave", function() {
-        overlay.style.display = "none";
-    })
-    overlay.addEventListener("click", function() {
-        alert("overlay clicked");
-    })
-} );
+})
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) {
+        return;
+    } else {
+        lightbox.classList.remove('active');
+    }
+})
